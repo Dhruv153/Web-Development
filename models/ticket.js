3408af9518127f34d5a3c317a848de7ee298b802
Dhruv Sharma <109913972+Dhruv153@users.noreply.github.com>
2023-12-15 15:23:06 +0530

    complete

---
+++ models/ticket.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const TicketSchema = new Schema({
+    author:String,
+    firstname:String,
+    lastname:String,
+    contact:String,
+    email:String,
+    adult_no:String,
+    children_no:String,
+    time_slot:String,
+    date:String
+});
+
+
+module.exports = mongoose.model('Ticket', TicketSchema);
